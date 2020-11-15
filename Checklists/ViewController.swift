585b9c9e5bbc846d94dd16d8cf6cf921c903119d
Kendra J. Moore <kendra.j.moore3443@gmail.com>
2020-11-15 13:12:00 -0500

    Initial Commit

---
+++ Checklists/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Checklists
+//
+//  Created by Kendra Mosley on 11/15/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
