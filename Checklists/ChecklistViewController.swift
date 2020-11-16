980952422cb515d550d07c0837dee6394145c2de
Kendra J. Moore <kendra.j.moore3443@gmail.com>
2020-11-16 12:18:11 -0500

    need to complete 9.4

---
+++ Checklists/ChecklistViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  Checklists
+//
+//  Created by Kendra Mosley on 11/15/20.
+//
+
+import UIKit
+
+class ChecklistViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    // MARK: - Table View Data Source
+    override func tableView(
+      _ tableView: UITableView,
+      numberOfRowsInSection section: Int
+    ) -> Int {
+      return 5
+    }
+
+    override func tableView(
+      _ tableView: UITableView,
+      cellForRowAt indexPath: IndexPath
+    ) -> UITableViewCell {
+      let cell = tableView.dequeueReusableCell(
+        withIdentifier: "ChecklistItem",
+        for: indexPath)
+
+      // Add the following code
+      let label = cell.viewWithTag(1000) as! UILabel
+
+        if indexPath.row % 5 == 0 {
+          label.text = "Walk the dog"
+        } else if indexPath.row % 5 == 1 {
+          label.text = "Brush my teeth"
+        } else if indexPath.row % 5 == 2 {
+          label.text = "Learn iOS development"
+        } else if indexPath.row % 5 == 3 {
+          label.text = "Soccer practice"
+        } else if indexPath.row % 5 == 4 {
+          label.text = "Eat ice cream"
+        }
+
+      // End of new code block
+
+        return cell
+        
+     }
+
+
+
+}
+
